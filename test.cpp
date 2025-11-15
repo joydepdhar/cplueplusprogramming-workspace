24a7b8206eb759013a479ad37088c223796f04f8
joydepdhar <joydepdhar138@gmail.com>
2025-11-16 00:37:20 +0600

    new code added

--- test.cpp
+++ test.cpp
@@ -1,5 +1,12 @@
-#include<stdio.h>
+#include<iostream>
 using namespace std;
 int main(){
-    cout("Hello");
+    // cout<<"Hello World"<<endl;
+    int x=10;
+    cout<<x<<endl;
+    char c='A';
+    double b=34.56;
+    cout<<"Char: "<<c<<endl;
+    cout<<"Double: "<<b<<endl;
+    return 0;
 }
