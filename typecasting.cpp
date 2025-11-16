6d7c6b7fcedcdfb3c875e60907eca372031068fb
joydepdhar <joydepdhar138@gmail.com>
2025-11-16 08:51:24 +0600

    new learning added

--- typecasting.cpp
+++ typecasting.cpp
@@ -1,7 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int x;
-    cin>>x;
-    cout<<x;
+
+int main() {
+    // int x;
+    // cin>>x;
+    // cout<<"output: "<<x;
+    // printing ascii value
+    char x='A';
+    int a=x;
+    cout<<a<<endl;
+    // type casting
+    int y=65;
+    cout<<(char)y<<endl;
+    return 0;
 }
