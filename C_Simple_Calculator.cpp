5e7074a9b55b4452a37df2452802c1980d67e505
joydepdhar <joydepdhar138@gmail.com>
2025-11-18 16:55:35 +0600

    sup

---
+++ C_Simple_Calculator.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long x,y,z;
+    cin>>x>>y;
+    cout<<x<<" + "<<y<<" = "<<x+y<<endl;
+    cout<<x<<" * "<<y<<" = "<<x*y<<endl;
+    cout<<x<<" - "<<y<<" = "<<x-y<<endl;
+    return 0;
+}
---
+++ E_Max.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    long long arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    long long mx=arr[0];
+      for (int i = 1; i < n; i++) {
+        mx = max(mx, arr[i]);
+    }
+    cout<<mx;
+    return 0;
+}
