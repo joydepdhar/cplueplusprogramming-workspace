36eb569db06ae1c560cf5f4702eef25616060abe
joydepdhar <joydepdhar138@gmail.com>
2025-11-25 22:05:06 +0600

    function

--- ifelse.cpp
+++ ifelse.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    int x=6;
-    if(x%2==0){
+void even(int num){
+    if(num%2==0){
         cout<<"Even\n";
     }else{
-        cout<<"Odd\n";
+        cout<<"odd\n";
     }
+}
+int main() {
+    int x=6;
+    even(x);
     return 0;
 }
