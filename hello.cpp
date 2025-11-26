c6fae27221c11c9c8d4b3a4c03a7b745c1c16458
joydepdhar <joydepdhar138@gmail.com>
2025-11-26 22:11:17 +0600

    new practice added

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+void printHello(string name){
+    cout<<"Hello, "<<name<<endl;
+}
 int main() {
-    cout<<"Welcome to CPluePlue Programming";
+    // cout<<"Welcome to CPluePlue Programming";
+    string name;
+    getline(cin,name);
+    printHello(name);
     return 0;
 }
