1a62cdf4519e4fe928e8be7cc43b352323398a2b
joydepdhar <joydepdhar138@gmail.com>
2025-11-28 22:48:31 +0600

    string

---
+++ string.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // string s="Hello";
+    // string s2="Hello";
+    // if(s==s2){
+    //     cout<<"same"<<endl;
+    // }else{
+    //     cout<<"Chi Noni Chi"<<endl;
+    // }
+    // cout<<s<<endl;
+    // return 0;
+    string s;
+    getline(cin, s);
+    cout<<s<<endl;
+}
