daacd5c15e1c7705bc6db263e4320cbbf3e23703
joydepdhar <joydepdhar138@gmail.com>
2025-11-29 23:42:24 +0600

    more about array object

--- arrayofobject.cpp
+++ arrayofobject.cpp
@@ -5,18 +5,38 @@ class Student{
     string name;
     int cls;
     int roll;
+    int marks;
 };
 
 
 int main() {
+    // int n;
+    // cin>>n;
+    // cin.ignore();
+    // Student a[n];
+    // for(int i=0;i<n;i++){
+    //     getline(cin,a[i].name);
+    //     cin.ignore();
+    //     cin>>a[i].cls>>a[i].roll;
+    // }  
+    // for(int i=0;i<n;i++){
+    //     cout<<a[i].name<<" "<<a[i].cls<<" "<<a[i].roll<<endl;
+    // }
     int n;
     cin>>n;
     Student a[n];
     for(int i=0;i<n;i++){
-        cin>>a[i].name>>a[i].cls>>a[i].roll;
+        // getline(cin,a[i].name);
+        // cin.ignore();
+        cin>>a[i].name>>a[i].cls>>a[i].roll>>a[i].marks;
     }  
+    int mn=INT_MAX;
     for(int i=0;i<n;i++){
-        cout<<a[i].name<<" "<<a[i].cls<<" "<<a[i].roll<<endl;
+        mn=min(a[i].marks,mn);
     }
+    // for(int i=0;i<n;i++){
+    //     cout<<a[i].name<<" "<<a[i].cls<<" "<<a[i].roll<<endl;
+    // }
+    cout<<mn<<endl;
     return 0;
 }
