a0c8da5ff7a269234f457a519dce217de5d2ba94
joydepdhar <joydepdhar138@gmail.com>
2025-12-11 11:25:29 +0600

    problem solving

--- C_Simple_Calculator.cpp
+++ C_Simple_Calculator.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
     long long x,y,z;
+    cout<<"Enter X,Y,Z";
     cin>>x>>y;
     cout<<x<<" + "<<y<<" = "<<x+y<<endl;
     cout<<x<<" * "<<y<<" = "<<x*y<<endl;
