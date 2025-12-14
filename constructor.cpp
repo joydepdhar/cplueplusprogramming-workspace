1c7d227a670b594a2051685d57ad7cf0bf1cb049
joydepdhar <joydepdhar138@gmail.com>
2025-12-14 18:01:49 +0600

    error

--- constructor.cpp
+++ constructor.cpp
@@ -12,12 +12,12 @@ class Student{
     }
 };
 int main() {
-    // Student rahim;
-    // rahim.roll=45;
-    // rahim.cls=5;
-    // rahim.gpa=4.10;
-    Student rahim(1,5,5.00);
-    Student karim(2,5,4.99);
+    Student rahim;
+    rahim.roll=45;
+    rahim.cls=5;
+    rahim.gpa=4.10;
+    // Student rahim(1,5,5.00);
+    // Student karim(2,5,4.99);
     cout<<rahim.roll<<" "<<rahim.cls<<" "<<rahim.gpa<<endl;
     cout<<karim.roll<<" "<<karim.cls<<" "<<karim.gpa<<endl;
     
