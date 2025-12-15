383e6141f145f99680519e246ecf3ae1b4dac3be
joydepdhar <joydepdhar138@gmail.com>
2025-12-15 14:59:27 +0600

    new line add

--- reversefunction.cpp
+++ reversefunction.cpp
@@ -17,8 +17,8 @@ int main() {
     // reverse(s.begin(),s.end());
     // cout<<s<<endl;
     string s = "hello";
-for (char ch : s) {
-cout << ch << " ";
-}
+        for (char ch : s) {
+        cout << ch << " ";
+    }
     return 0;
 }
